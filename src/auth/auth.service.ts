4fc814ca8b197fa6621fd5af29925d84e64d220b
Davi Oliveira <davicicfaculdade@gmail.com>
2023-07-13 10:18:27 -0300

    Changing imports that use src/ as base to relative

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,7 +6,7 @@ import {
 } from '@nestjs/common/';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/users/schemas/user.schema';
+import { User } from '../users/schemas/user.schema';
 import { CreateUserDto } from './dto/createUser.dto';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
